b147ce4846ff7b42cd59ea9de1babcf83dad01ff
MasterAniket <aniketmaji265@gmail.com>
2023-06-03 06:04:30 +0530

    Commited

---
+++ Flutter Development/i_am_rich/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IAmRich',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue[100],
+        title: const Center(child: Text('I Am Rich')),
+      ),
+      body: Center(
+        child: Container(
+          height: 165.0,
+          width: 165.0,
+          child: Image.asset('images/diamond.png'),
+        ),
+      ),
+    );
+  }
+}
